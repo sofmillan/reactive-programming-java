bc203c26d234b34da0e09a7865ff283dee3c9a8b
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-04-09 15:38:30 -0500

    Log

--- Mastering-Java-Programming/src/main/java/com/sec02/Lec05FluxRange.java
+++ Mastering-Java-Programming/src/main/java/com/sec02/Lec05FluxRange.java
@@ -6,6 +6,7 @@ import reactor.core.publisher.Flux;
 public class Lec05FluxRange {
     public static void main(String[] args) {
         Flux.range(1,10)
+                .log()
                 .map(i -> Util.faker().name().fullName()  )
                 .subscribe(
                         Util.onNext()
