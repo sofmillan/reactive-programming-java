ff6f08a14f6a4fd4adee6503921e49af2121d1af
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-04-18 15:32:47 -0500

    Flux to Mono

--- Mastering-Java-Programming/src/main/java/com/sec02/Lec09FluxFromMono.java
+++ Mastering-Java-Programming/src/main/java/com/sec02/Lec09FluxFromMono.java
@@ -6,10 +6,15 @@ import reactor.core.publisher.Mono;
 
 public class Lec09FluxFromMono {
     public static void main(String[] args) {
-        Mono<String> mono = Mono.just("a");
+    /*    Mono<String> mono = Mono.just("a");
 
         Flux.from(mono)
-                .subscribe(Util.onNext());
+                .subscribe(Util.onNext());*/
+
+        Flux.range(1, 10)
+                .filter(i->i>3)
+                .next() //First item
+                .subscribe(Util.onNext(),Util.onError(), Util.onComplete());
     }
 
     private static void doSomething(Flux<String> flux){
