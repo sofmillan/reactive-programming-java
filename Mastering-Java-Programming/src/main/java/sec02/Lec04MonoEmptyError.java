b8d5ce0b5eff89bdbc15bbceec2e6ade39284951
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-06-24 22:52:57 -0500

    Mono empty / Mono error

---
+++ Mastering-Java-Programming/src/main/java/sec02/Lec04MonoEmptyError.java
@@ -0,0 +1,18 @@
+package sec02;
+
+import common.Util;
+import reactor.core.publisher.Mono;
+
+public class Lec04MonoEmptyError {
+    public static void main(String[] args) {
+        getUserName(3).subscribe(Util.subcriber());
+    }
+
+    private static Mono<String> getUserName(int userId){
+        return switch(userId){
+            case 1 -> Mono.just("ricky");
+            case 2 -> Mono.empty();
+            default -> Mono.error(new IllegalArgumentException("Invalid input"));
+        };
+    }
+}
